72d8453a8c296f796e9816bde66854b224153bcb
Dhanesh6864 <112810784+Dhanesh6864@users.noreply.github.com>
2022-09-06 14:39:12 +0530

    Add files via upload

--- noofwordstring.py
+++ noofwordstring.py
@@ -6,5 +6,5 @@ print("The original string is : " + string)
  
 res = len(string.split())
  
-print("The number of words in string are : " + str(res))
+print("The number of words in string are : ", + str(res))
 print("The number of words in string are : ", len(string))
