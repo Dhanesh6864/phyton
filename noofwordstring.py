3f911c90357a9816f77f54f72b0ce84a93f14991
Dhanesh6864 <112810784+Dhanesh6864@users.noreply.github.com>
2022-09-06 13:52:40 +0530

    Add files via upload

---
+++ noofwordstring.py
@@ -0,0 +1,10 @@
+
+string =input('enter the string') 
+ 
+
+print("The original string is : " + string)
+ 
+res = len(string.split())
+ 
+print("The number of words in string are : " + str(res))
+print("The number of words in string are : ", len(string))
